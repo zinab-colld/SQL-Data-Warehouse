85c17933737c818be5db3f963a2ccefd75a95201
Zinab-Dev <147951103+zinab-colld@users.noreply.github.com>
2026-07-15 01:20:30 +0300

    Update gold_layer.sql

--- gold_layer.sql
+++ gold_layer.sql
@@ -1,1 +1,63 @@
+CREATE DATABASE IF NOT EXISTS dw_gold;
 
+CREATE OR REPLACE VIEW dw_gold.dim_customers AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY ci.cst_id) AS customer_key,
+    ci.cst_id                              AS customer_id,
+    ci.cst_key                             AS customer_number,
+    ci.cst_firstname                       AS first_name,
+    ci.cst_lastname                        AS last_name,
+    la.cntry                               AS country,
+    ci.cst_marital_status                  AS marital_status,
+    CASE 
+        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+        ELSE COALESCE(ca.gen, 'n/a')
+    END                                    AS gender,
+    ca.bdate                               AS birthdate,
+    ci.cst_create_date                     AS create_date
+FROM dw_silver.crm_cust_info ci
+LEFT JOIN dw_silver.erp_cust_az12 ca 
+    ON ci.cst_key = ca.cid
+LEFT JOIN dw_silver.erp_loc_a101 la   
+    ON ci.cst_key = la.cid;
+
+CREATE OR REPLACE VIEW dw_gold.dim_products AS
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+    pn.prd_id                                                AS product_id,
+    pn.prd_key                                               AS product_number,
+    pn.prd_nm                                                AS product_name,
+    pn.cat_id                                                AS category_id,
+    pc.cat                                                   AS category,
+    pc.subcat                                                AS subcategory,
+    pc.maintenance                                           AS maintenance,
+    pn.prd_cost                                              AS cost,
+    pn.prd_line                                              AS product_line,
+    pn.prd_start_dt                                          AS start_date
+FROM dw_silver.crm_prd_info pn
+LEFT JOIN dw_silver.erp_px_cat_g1v2 pc 
+    ON pn.cat_id = pc.id;
+
+CREATE OR REPLACE VIEW dw_gold.fact_sales AS
+SELECT 
+    sd.sls_ord_num     AS order_number,
+    pr.product_key,
+    cu.customer_key,
+    sd.sls_order_dt    AS order_date,
+    sd.sls_ship_dt     AS shipping_date,
+    sd.sls_due_dt      AS due_date,
+    sd.sls_sales       AS sales_amount,
+    sd.sls_qty         AS quantity,
+    sd.sls_price       AS price
+FROM dw_silver.crm_sales_details sd
+LEFT JOIN dw_gold.dim_products pr 
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN dw_gold.dim_customers cu 
+    ON sd.sls_cust_id = cu.customer_id;
+
+SELECT * FROM dw_gold.dim_customers LIMIT 10;
+
+SELECT * 
+FROM dw_gold.fact_sales f
+LEFT JOIN dw_gold.dim_products p ON p.product_key = f.product_key
+WHERE f.product_key IS NULL;
