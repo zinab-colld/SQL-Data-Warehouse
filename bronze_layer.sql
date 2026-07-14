d072fa0c895b78766997d4500fcf585dfe50d46b
Zinab-Dev <147951103+zinab-colld@users.noreply.github.com>
2026-07-15 01:17:44 +0300

    Update bronze_layer.sql

--- bronze_layer.sql
+++ bronze_layer.sql
@@ -1,1 +1,58 @@
+CREATE DATABASE IF NOT EXISTS dw_bronze;
+USE dw_bronze;
 
+DROP TABLE IF EXISTS dw_bronze.crm_cust_info;
+CREATE TABLE dw_bronze.crm_cust_info (
+    cst_id             INT,
+    cst_key            VARCHAR(50),
+    cst_firstname      VARCHAR(50),
+    cst_lastname       VARCHAR(50),
+    cst_material_status VARCHAR(50),
+    cst_gndr           VARCHAR(50),
+    cst_create_date    DATE
+);
+
+DROP TABLE IF EXISTS dw_bronze.crm_prd_info;
+CREATE TABLE dw_bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      VARCHAR(50),
+    prd_nm       VARCHAR(50),
+    prd_cost     INT,
+    prd_line     VARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+
+DROP TABLE IF EXISTS dw_bronze.crm_sales_details;
+CREATE TABLE dw_bronze.crm_sales_details (
+    sls_ord_num  VARCHAR(50),
+    sls_prd_key  VARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+
+DROP TABLE IF EXISTS dw_bronze.erp_cust_az12;
+CREATE TABLE dw_bronze.erp_cust_az12 (
+    cid   VARCHAR(50),
+    bdate DATE,
+    gen   VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS dw_bronze.erp_loc_a101;
+CREATE TABLE dw_bronze.erp_loc_a101 (
+    cid   VARCHAR(50),
+    cntry VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS dw_bronze.erp_px_cat_g1v2;
+CREATE TABLE dw_bronze.erp_px_cat_g1v2 (
+    id          VARCHAR(50),
+    cat         VARCHAR(50),
+    subcat      VARCHAR(50),
+    maintenance VARCHAR(50)
+);
